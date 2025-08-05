14fc696080373a35457512001ad6ef4bb946f9c6
kenishajudith <kenishajudith03@gmail.com>
2025-08-05 10:43:45 +0530

    Add files via upload

---
+++ Factorial.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int factorial(int n, int fact)
+
+{
+
+for(int i=1;i<=n;i++)
+
+fact=fact*i;
+
+return fact;
+
+}
+
+int main()
+
+{
+
+int n;
+
+int fact=1;
+
+printf("Enter a number: ");
+
+scanf("%d",&n);
+
+int res-factorial (n,fact);
+
+printf("\n%d",res);
+
+return 0;
+
+}
