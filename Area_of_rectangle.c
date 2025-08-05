2f863ded694316598d6d9c99e223514577ffdda7
kenishajudith <kenishajudith03@gmail.com>
2025-08-05 10:44:39 +0530

    Add files via upload

---
+++ Area_of_rectangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int rec(int 1,int b)
+{
+int area=1*b;
+return area;
+}
+int main()
+int 1,b;
+printf("Enter the value of 1 and b: ");
+scanf("%d %d ",&l,&b);
+int res=rec(1,b);
+printf("\nArea of rectangle: %d",res);
+return 0;
+}
